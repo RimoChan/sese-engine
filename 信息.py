c6c97916c93e3dded4b4b93aeed4df690cd204ad
RimoChan <RimoChan@stu.xmu.edu.cn>
2022-01-17 22:49:02 +0800

    没有就不加载

--- 信息.py
+++ 信息.py
@@ -1,10 +1,14 @@
 import json
+from pathlib import Path
 
 
 def 繁荣表():
-    with open('savedata/繁荣.json', encoding='utf8') as f:
-        d = json.load(f)
-    总 = sum(d.values())
-    d = {k: v/总*300000 for k, v in d.items()}
-    d = {k: v for k, v in d.items() if v > 0.2}
-    return d
+    if Path('savedata/繁荣.json').is_file():
+        with open('savedata/繁荣.json', encoding='utf8') as f:
+            d = json.load(f)
+        总 = sum(d.values())
+        d = {k: v/总*300000 for k, v in d.items()}
+        d = {k: v for k, v in d.items() if v > 0.2}
+        return d
+    else:
+        return {}
