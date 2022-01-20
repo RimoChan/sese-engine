dcc8afa75b8dcbac216635ec183e3fecd2c2a8e2
RimoChan <RimoChan@stu.xmu.edu.cn>
2022-01-20 21:35:08 +0800

    父域名会获得子域名的繁荣

--- 信息.py
+++ 信息.py
@@ -3,12 +3,18 @@ from pathlib import Path
 
 
 def 繁荣表():
-    if Path('savedata/繁荣.json').is_file():
-        with open('savedata/繁荣.json', encoding='utf8') as f:
-            d = json.load(f)
-        总 = sum(d.values())
-        d = {k: v/总*300000 for k, v in d.items()}
-        d = {k: v for k, v in d.items() if v > 0.2}
-        return d
-    else:
+    if not Path('savedata/繁荣.json').is_file():
         return {}
+    with open('savedata/繁荣.json', encoding='utf8') as f:
+        d = json.load(f)
+    总 = sum(d.values())
+    d = {k: v/总*300000 for k, v in d.items()}
+    for k, v in d.items():
+        now = k
+        while True:
+            now = '.'.join(now.split('.')[1:])
+            if now not in d:
+                break
+            if d[now] < v:
+                d[now] = v
+    return d
