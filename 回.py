c5550a648471b5bbf9536bcaed6e866fc1be3286
RimoChan <RimoChan@stu.xmu.edu.cn>
2022-05-28 12:55:53 +0800

    抑制多个来自相同ip的反向链接

--- 回.py
+++ 回.py
@@ -3,7 +3,7 @@ import time
 import random
 import logging
 import datetime
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Optional, List
 
 from tqdm import tqdm
 from utils import 分解, netloc
@@ -25,10 +25,17 @@ def 域名相似(a: str, b: str) -> float:
     return len(a & b) / max(1, len(a | b))
 
 
+def ip字符串(ip_list: Optional[List[str]]) -> str:
+    ip_list = sorted(ip_list or [])
+    ip_list = [i[:i.rfind('.')] for i in ip_list]
+    return ','.join(ip_list)
+
+
 def 计数() -> Tuple[Dict[str, int], Dict[str, int]]:
     网站之门 = 融合之门(存储位置/'网站之门')
     子域名个数 = {}
     模板个数 = {}
+    同ip个数 = {}
     for i, (k, v) in tqdm(enumerate(iter(网站之门.items())), desc='计数'):
         if (i+1) % 100_0000 == 0:
             模板个数 = {k: v for k, v in 模板个数.items() if v > 1}
@@ -36,23 +43,31 @@ def 计数() -> Tuple[Dict[str, int], Dict[str, int]]:
         子域名个数[超b] = 子域名个数.get(超b, 0) + 1
         if t:=v.get('结构'):
             模板个数[t] = 模板个数.get(t, 0) + 1
+        if ip:=v.get('ip'):
+            ip_str = ip字符串(ip)
+            同ip个数[ip_str] = 同ip个数.get(ip_str, 0) + 1
     print(f'一级域名个数: {len(子域名个数)}')
     子域名个数 = {k: v for k, v in 子域名个数.items() if v >= 4}
     模板个数 = {k: v for k, v in 模板个数.items() if v >= 4}
-    return 子域名个数, 模板个数
+    同ip个数 = {k: v for k, v in 同ip个数.items() if v >= 4}
+    return 子域名个数, 模板个数, 同ip个数
 
 
 def 刷新():
-    子域名个数, 模板个数 = 计数()
+    子域名个数, 模板个数, 同ip个数 = 计数()
     with open(存储位置/'子域名个数.json', 'w', encoding='utf8') as f:
         f.write(json.dumps(子域名个数, ensure_ascii=False, indent=2))
     with open(存储位置/'模板个数.json', 'w', encoding='utf8') as f:
         f.write(json.dumps(模板个数, ensure_ascii=False, indent=2))
+    with open(存储位置/'同ip个数.json', 'w', encoding='utf8') as f:
+        f.write(json.dumps(同ip个数, ensure_ascii=False, indent=2))
     网站之门 = 融合之门(存储位置/'网站之门')
+    ip来源 = {}
     d = {}
     for i, (k, v) in tqdm(enumerate(iter(网站之门.items())), desc='计算反向链接'):
         if (i+1) % 100_0000 == 0:
             d = {k: v for k, v in d.items() if v >= 0.04}
+            ip来源 = {k: v for k, v in ip来源.items() if v >= 0.04}
         a = v.get('链接')
         if not a:
             continue
@@ -81,12 +96,20 @@ def 刷新():
                 else:
                     xd[x] += 真w
         倍 = 时间倍 * 域名倍 * 协议倍
+        ip_str = ip字符串(v.get('ip'))
         for x, w in xd.items():
             w = min(w, 0.15) * 倍
             if x not in d:
                 d[x] = w
             else:
+                if ip_str in 同ip个数 and d[x] > 0.2:
+                    key = f'{x}-{ip_str}'
+                    if ip来源.get(key, 0) > 0.4:
+                        # print(key, ip来源[key], d[x])
+                        continue
+                    ip来源[key] = ip来源.get(key, 0) + w
                 d[x] += w
+
     print('好！')
     d = {k: v for k, v in d.items() if v > 0.16}
     d = dict(sorted(d.items()))
